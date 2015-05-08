0235aa2839b8affaa316c745f88e230af33bce2c
collectorD <collectorD@yandex.com>
2015-05-09 00:02:34 +0200

    Create collection.js

---
+++ collection.js
@@ -0,0 +1,9 @@
+[
+{ "moduleId" : "maps4all/Roadworks"},
+{ "moduleId" : "maps4all/parkopedia"},
+{ "moduleId" : "mapman100/Here"},
+{ "moduleId" : "jamesbond002/osm"},
+{ "moduleId" : "wazemap/waze"},
+{ "moduleId" : "wazemap/wikimap"},
+{ "moduleId" : "grabowCommuter/Speedometer"}
+]
